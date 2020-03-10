96eabb82c6993b1e9611fcf37dbbd4daa714b87d
Butterbroad <pyatochevskiy@gmail.com>
2020-03-10 11:10:48 +0300

    added scroll

--- app/js/main.js
+++ app/js/main.js
@@ -203,6 +203,21 @@ window.addEventListener('click', function () {
   succesModal.style.display = "none";
 });
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
+
 
 
 
